046dcf0417ae02704d04d431e40c47db4af670ca
FelipeQueFez <felipe.aparecido.ext@infracommerce.com.br>
2022-01-30 14:06:58 -0300

    flutter_bloc

--- lib/custom_utilities.dart
+++ lib/custom_utilities.dart
@@ -36,3 +36,4 @@ export 'package:equatable/equatable.dart';
 export 'package:share/share.dart';
 export 'package:url_launcher/url_launcher.dart';
 export 'package:get_it/get_it.dart';
+export 'package:flutter_bloc/flutter_bloc.dart';
