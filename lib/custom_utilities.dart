ec10bb05dbab539071fac1556170cdfe125ff7b6
FelipeQueFez <felipe.aparecido.ext@infracommerce.com.br>
2022-03-06 22:24:03 -0300

    more items in utilities package

--- lib/custom_utilities.dart
+++ lib/custom_utilities.dart
@@ -1,34 +1,6 @@
 library custom_utilities;
 
-export './ads/ads_config.dart';
-export './extensions/extension.dart';
-export 'utils/is_debug_mode.dart';
-export 'utils/utils.dart';
-export 'utils/graphql_utils.dart';
-export 'device/service/device_info_service.dart';
-export 'http_client/remote_client_repository.dart';
-
-//Logger
-export 'logger/enums/logger_type_enum.dart';
-export 'logger/models/logger_model.dart';
-export 'logger/repository/logger_repository.dart';
-export 'logger/service/logger_service.dart';
-
-//Bloc
-export 'bloc/default_state.dart';
-
-//Models
-export 'models/key_value.dart';
-export 'models/response_default_model.dart';
-
-//Services
-export 'services/launch_url_service.dart';
-export 'services/share_service.dart';
-
-//Authentication
-export 'authentication/models/user_app_base_model.dart';
-export 'authentication/custom_authentication_repository.dart';
-export 'authentication/exceptions/authentication_exceptions.dart';
+export './src/src.dart';
 
 //Packages
 export 'package:intl/intl.dart';
